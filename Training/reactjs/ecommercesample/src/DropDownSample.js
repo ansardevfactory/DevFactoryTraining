c01274efbac65639331f2fba53ec3171585935b7
ansardevfactory <104754771+ansardevfactory@users.noreply.github.com>
2022-05-24 12:12:07 +0530

    Update DropDownSample.js

--- Training/reactjs/ecommercesample/src/DropDownSample.js
+++ Training/reactjs/ecommercesample/src/DropDownSample.js
@@ -1,19 +1,30 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 export default function NewSample() {
   const [val, setValue] = useState("one");
+  const [localvariable, setLocalVariable] = useState("");
   const setValueChange = (e) => {
     setValue(e.target.value);
+    localStorage.setItem("variablename", "");
   };
-  const handleclick = () => {};
+  useEffect(() => {
+    setLocalVariable(localStorage.getItem("variablename"));
+  }, []);
+  const handleclick = () => {
+    setLocalVariable(localStorage.getItem("variablename"));
+  };
   return (
     <div>
       {val}
-      <select  value={val} onChange={(e) => setValueChange(e)}>
+      <br></br>
+      <select value={val} onChange={(e) => setValueChange(e)}>
         <option value={"select"}>select</option>
         <option value={"one"}>one</option>
         <option value={"two"}>Two</option>
       </select>
+      <br></br>
+      localStorage: {localvariable}
+      <button onClick={handleclick}>Click</button>
     </div>
   );
 }
