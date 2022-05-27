a51f4eb42236177433e199e3d88998be33271af1
ansardevfactory <104754771+ansardevfactory@users.noreply.github.com>
2022-05-27 17:41:12 +0530

    dropdown

--- Training/reactjs/usecallbacksample/src/Parent.js
+++ Training/reactjs/usecallbacksample/src/Parent.js
@@ -4,7 +4,8 @@ import Child from "./Child";
 const App = () => {
   const [count, setCount] = useState(0);
   const [todos, setTodos] = useState([]);
-
+  const [list, setList]=useState([{"id":1,"value":"one"},{"id":2,"value":"two"},{"id":3,"value":"three"}])
+const [val,setVal]=useState("one")
   const increment = () => {
     setCount((c) => c + 1);
   };
@@ -12,8 +13,17 @@ const App = () => {
     setTodos((t) => [...t, "New Todo"]);
   },[todos])
 
+  const handleChange=(e)=>{
+    console.log(e.target.value)
+  }
   return (
     <>
+    <label>AssignedTo</label>
+    <select value={val} onChange={(e)=>handleChange(e)} >
+      {list.map((itm)=>{
+        return <option value={itm.id}>{itm.value}</option>
+      })}
+      </select>
       <Child todos={todos} addTodo={addTodo} />
       <hr />
       <div>
