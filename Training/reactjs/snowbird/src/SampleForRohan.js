8b2bfae8bb8b4299dff901a6063aa495a13871db
ansardevfactory <104754771+ansardevfactory@users.noreply.github.com>
2022-07-13 12:21:09 +0530

    css sample

---
+++ Training/reactjs/snowbird/src/SampleForRohan.js
@@ -0,0 +1,112 @@
+import React from "react";
+import "./newstyle.css";
+import {
+  AiFillAlipayCircle,
+  AiFillFacebook,
+  AiFillTwitterSquare,
+} from "react-icons/ai";
+export default function SampleForRohan() {
+  return (
+    <>
+      <div className="row1">
+        <div className="left">
+          <label>Test</label> <label>Abcd</label>
+        </div>
+        <div className="right">
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+        </div>
+      </div>
+      <div className="row2">
+        <div className="products">
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label> <label>Test</label>{" "}
+          <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label> <label>Test</label>{" "}
+          <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label> <label>Test</label>{" "}
+          <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label> <label>Test</label>{" "}
+          <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label> <label>Test</label>{" "}
+          <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label> <label>Test</label>{" "}
+          <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label> <label>Test</label>{" "}
+          <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label> <label>Test</label>{" "}
+          <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label> <label>Test</label>{" "}
+          <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label> <label>Test</label>{" "}
+          <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label> <label>Test</label>{" "}
+          <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label> <label>Test</label>{" "}
+          <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label> <label>Test</label>{" "}
+          <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+          <label>Test</label> <label>Abcd</label>
+        </div>
+      </div>
+      <div className="row3">
+        <div className="row3_left">
+          <div className="row3_left_logo">
+            <AiFillAlipayCircle />
+          </div>
+          <div className="row3_left_text">
+            <label className="head">Heading One</label>
+            <label>Below text</label>
+          </div>
+        </div>
+        <div className="row3_right">
+          <div className="row3_right_top">
+            <AiFillFacebook />
+            <AiFillTwitterSquare />
+            <AiFillFacebook />
+            <AiFillTwitterSquare />
+          </div>
+          <div className="row3_right_bottom">
+            <ul>
+              <li>HOME</li>
+              <li>OUR VISION</li>
+              <li>OUR MISSION</li>
+              <li>ABOUT US</li>
+              <li>CONTACT US</li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
