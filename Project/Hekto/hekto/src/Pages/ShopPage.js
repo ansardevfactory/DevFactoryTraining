8a9420dc2c9717bf41af49741e9650d41eef5796
ansardevfactory <104754771+ansardevfactory@users.noreply.github.com>
2022-09-12 18:03:47 +0530

    g

--- Project/Hekto/hekto/src/Pages/ShopPage.js
+++ Project/Hekto/hekto/src/Pages/ShopPage.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import Filterhorizontal from '../Components/Filterhorizontal'
+import Header from '../Components/Header'
+import Footer from '../Components/Footer'
 import Subbanner from '../Components/Subbanner'
 
 export default function ShopPage() {
   return (
     <>
+    <Header/>
     <Subbanner/>
     <Filterhorizontal/>
+    <Footer/>
     </>
   )
 }
