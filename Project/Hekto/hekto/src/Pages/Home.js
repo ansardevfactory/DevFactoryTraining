21562cac32d2f5dd0015d577ce637f751040e41c
ansardevfactory <104754771+ansardevfactory@users.noreply.github.com>
2022-09-13 10:58:44 +0530

    g

--- Project/Hekto/hekto/src/Pages/Home.js
+++ Project/Hekto/hekto/src/Pages/Home.js
@@ -8,19 +8,61 @@ import Discountitem from "../Components/Discountitem";
 import Latestblog from "../Components/Latestblog";
 import BrandList from "../Components/BrandList";
 import InBetweenBanner1 from "../Components/InBetweenBanner1";
+import "./newstyle.css";
+import imgvar from "../Components/Images/image1.png";
+import imgvar2 from "../Components/Images/image9.png";
 
 export default function Home() {
   return (
     <>
+      <div className="products">
+        <SingleProduct
+          title={"FirstTitle"}
+          first={"InsideFirst"}
+          second={"InsideSecond"}
+          localimg={imgvar}
+        />
+        <SingleProduct
+          title={"SecondTitle"}
+          first={"InsideFirst"}
+          second={"InsideSecond"}
+          localimg={imgvar2}
+        />
+        <SingleProduct
+          title={"ThirdTitle"}
+          first={"InsideFirst"}
+          second={"InsideSecond"}
+          localimg={imgvar}
+        />
+        <SingleProduct
+          title={"FourthTitle"}
+          first={"InsideFirst"}
+          second={"InsideSecond"}
+          localimg={imgvar2}
+        />
+      </div>
       <Header />
-      <Mainbanner/>
-      <OfferList/>
-      <InBetweenBanner1/>
-      <Discountitem/>
-      <InBetweenBanner2/>
-      <BrandList/>
-      <Latestblog/>
+      <Mainbanner />
+      <OfferList />
+      <InBetweenBanner1 />
+      <Discountitem />
+      <InBetweenBanner2 />
+      <BrandList />
+      <Latestblog />
       <Footer />
+    </>
+  );
+}
+
+function SingleProduct({ title, first, second,localimg }) {
+  return (
+    <>
+      <div className="singleproduct_outer">
+        <img src={localimg} />
+        <div>{title}</div>
+        <div>{first}</div>
+        <div>{second}</div>
+      </div>
     </>
   );
 }
